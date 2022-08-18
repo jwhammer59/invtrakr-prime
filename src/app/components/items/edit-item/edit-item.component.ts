f73d2690bf990931d8a8fe6aaa85fe5477dd90f9
jwhammer59 <jeff@jhammermedia>
2022-08-18 15:56:00 -0400

    add item detail misc ui tweaks

--- src/app/components/items/edit-item/edit-item.component.ts
+++ src/app/components/items/edit-item/edit-item.component.ts
@@ -1,15 +1,171 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Item } from 'src/app/models/Item';
+import { ItemsService } from 'src/app/services/items.service';
+
+import { Room } from 'src/app/models/Room';
+import { RoomsService } from 'src/app/services/rooms.service';
+
+import { Store } from 'src/app/models/Store';
+import { StoresService } from 'src/app/services/stores.service';
+
+import { PaymentType } from 'src/app/models/PaymentType';
+import { PaymentTypesService } from 'src/app/services/payment-types.service';
+
+import { MessageService, PrimeNGConfig } from 'primeng/api';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-edit-item',
   templateUrl: './edit-item.component.html',
-  styleUrls: ['./edit-item.component.css']
+  styleUrls: ['./edit-item.component.css'],
 })
 export class EditItemComponent implements OnInit {
+  submitted: boolean = false;
 
-  constructor() { }
+  editItemForm!: FormGroup;
+  id!: string;
+  userRef: any;
 
-  ngOnInit(): void {
+  paymentTypes$!: Observable<PaymentType[]>;
+  paymentTypesArray: PaymentType[] = [];
+  paymentTypesNameArray: String[] = [];
+
+  rooms$!: Observable<Room[]>;
+  roomsArray: Room[] = [];
+  roomsNameArray: String[] = [];
+
+  stores$!: Observable<Store[]>;
+  storesArray: Store[] = [];
+  storesNameArray: String[] = [];
+
+  constructor(
+    private itemsService: ItemsService,
+    private roomsService: RoomsService,
+    private storesService: StoresService,
+    private paymentTypesService: PaymentTypesService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private messageService: MessageService,
+    private primengConfig: PrimeNGConfig
+  ) {
+    this.editItemForm = fb.group({
+      itemOwnerID: this.id,
+      itemDwellingName: '',
+      itemName: ['', Validators.required],
+      itemDesc: ['', Validators.required],
+      itemMfg: '',
+      itemModelNum: '',
+      itemSerialNum: '',
+      itemQty: ['', Validators.required],
+      itemExtWarranty: false,
+      itemPurchaseDate: ['', Validators.required],
+      itemPurchasePrice: '',
+      itemVendor: '',
+      itemPaymentType: '',
+      itemRoom: ['', Validators.required],
+      itemNote: '',
+    });
   }
 
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+    this.id = this.route.snapshot.params['id'];
+
+    this.itemsService.getItem(this.id).subscribe((res) => {
+      this.userRef = res;
+
+      this.editItemForm = this.fb.group({
+        itemOwnerID: [this.userRef.itemOwnerID],
+        itemDwellingName: [this.userRef.itemDwellingName],
+        itemName: [this.userRef.itemName],
+        itemDesc: [this.userRef.itemDesc],
+        itemMfg: [this.userRef.itemMfg],
+        itemModelNum: [this.userRef.itemModelNum],
+        itemSerialNum: [this.userRef.itemSerialNum],
+        itemQty: [this.userRef.itemQty],
+        itemExtWarranty: [this.userRef.itemExtWarranty],
+        itemPurchaseDate: [this.userRef.itemPurchaseDate],
+        itemPurchasePrice: [this.userRef.itemPurchasePrice],
+        itemVendor: [this.userRef.itemVendor],
+        itemPaymentType: [this.userRef.itemPaymentType],
+        itemRoom: [this.userRef.itemRoom],
+        itemNote: [this.userRef.itemNote],
+      });
+    });
+
+    this.paymentTypes$ = this.paymentTypesService.getPaymentTypes();
+    this.paymentTypes$.subscribe((data) => {
+      this.paymentTypesArray = data;
+      this.getPaymentTypesName(this.paymentTypesArray);
+    });
+
+    this.rooms$ = this.roomsService.getRooms();
+    this.rooms$.subscribe((data) => {
+      this.roomsArray = data;
+      this.getRoomsName(this.roomsArray);
+    });
+
+    this.stores$ = this.storesService.getStores();
+    this.stores$.subscribe((data) => {
+      this.storesArray = data;
+      this.getStoresName(this.storesArray);
+    });
+  }
+
+  getPaymentTypesName(data: PaymentType[]) {
+    this.paymentTypesNameArray = [];
+    data.map((el) => {
+      let pTypeToStore = '';
+      pTypeToStore = el.paymentTypeName;
+      this.paymentTypesNameArray.push(pTypeToStore);
+    });
+  }
+
+  getRoomsName(data: Room[]) {
+    this.roomsNameArray = [];
+    data.map((el) => {
+      let roomToStore = '';
+      roomToStore = el.roomName;
+      this.roomsNameArray.push(roomToStore);
+    });
+  }
+
+  getStoresName(data: Store[]) {
+    this.storesNameArray = [];
+    data.map((el) => {
+      let storeToStore = '';
+      storeToStore = el.storeName;
+      this.storesNameArray.push(storeToStore);
+    });
+  }
+
+  onSubmit({ value, valid }: { value: Item; valid: boolean }) {
+    this.submitted = true;
+    let tempID = this.id;
+
+    if (!valid) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Form Is Invalid',
+        detail: 'Check form for errors!',
+        life: 3000,
+      });
+    } else {
+      this.itemsService.updateItem(value, tempID);
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Item Updated!',
+        life: 3000,
+      });
+      this.submitted = false;
+      setTimeout(() => {
+        this.router.navigate(['/items']);
+      }, 2000);
+    }
+  }
 }
--- src/app/components/items/item-detail/item-detail.component.ts
+++ src/app/components/items/item-detail/item-detail.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Item } from 'src/app/models/Item';
+import { ItemsService } from 'src/app/services/items.service';
+
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
-  styleUrls: ['./item-detail.component.css']
+  styleUrls: ['./item-detail.component.css'],
 })
 export class ItemDetailComponent implements OnInit {
+  id: string = '';
 
-  constructor() { }
+  item: Item = {
+    itemOwnerID: '',
+    itemDwellingName: '',
+    itemName: '',
+    itemDesc: '',
+    itemMfg: '',
+    itemModelNum: '',
+    itemSerialNum: '',
+    itemQty: '',
+    itemExtWarranty: false,
+    itemPurchaseDate: '',
+    itemPurchasePrice: '',
+    itemVendor: '',
+    itemPaymentType: '',
+    itemRoom: '',
+    itemNote: '',
+  };
+
+  constructor(
+    private itemsService: ItemsService,
+    private route: ActivatedRoute,
+    private primengConfig: PrimeNGConfig
+  ) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.primengConfig.ripple = true;
+    this.itemsService.getItem(this.id).subscribe((item) => {
+      this.item = item;
+    });
   }
-
 }
